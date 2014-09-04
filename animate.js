ee6deef29fac16993ca4204bf51524ad9cb73574
ATalaba <andrei_talaba@yahoo.com>
2014-09-04 14:19:08 -0400

    Pages

--- animate.js
+++ animate.js
@@ -1,12 +1,7 @@
 $(document).ready(function() {
-	$('#openthepage').click(function() {
-		$('#openthepage').animate({
-			bottom: 1000,
-			opacity: 0.5
-		}, 2000, function(){})
-		$('img#openthepage').animate({
-			bottom: 1000,
-			opacity: 0.5
-		}, 2000, function(){})
+	$('.poem').hover(function() {
+		$('#'+this.id+'more').show(700);
+	}, function() {
+		$('#'+this.id+'more').hide(700);
 	});
 });
