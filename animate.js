29c212226bd03ef31d70b8c6f806739b89835e09
ATalaba <andrei_talaba@yahoo.com>
2014-09-10 22:58:26 -0400

    Added new poem, and fixed responsive legend

--- animate.js
+++ animate.js
@@ -1,3 +1,10 @@
+/*$(document).ready(function() {
+	$('.hover').hover(function() {
+		$('#'+$(this).parent().parent().attr('id')+'more').show(700);
+	}, function() {
+		$('#'+$(this).parent().parent().attr('id')+'more').hide(700);
+	});
+})*/
 $(document).ready(function() {
 	$('.poem').hover(function() {
 		$('#'+this.id+'more').show(700);
